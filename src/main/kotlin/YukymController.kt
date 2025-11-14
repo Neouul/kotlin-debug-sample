864cb888887ddb6d714d67fabe7061a12c6aaa9b
Neouul <fluctu0q0@gmail.com>
2025-11-14 19:09:15 +0900

    Refactor: 조금 리팩토링

--- src/main/kotlin/YukymController.kt
+++ src/main/kotlin/YukymController.kt
@@ -13,60 +13,56 @@ class YukymController {
     lateinit var nowTime: String
 
     fun getTyA(): String {
-        val timeDataOne = _getTimeDataOne(nowDate)
+        val timeDataOne = _getTimeDataOne()
 
-        if (timeDataOne.isNotEmpty()) {
-            // nowTime은 무조건 "갑자1국"
-            nowTime = timeDataOne.first().ty1
+        nowTime = timeDataOne.ty1
 
-            // nowDate의 month값이 이상해서 항상 else로 처리됨 => "갑자1국"이 리턴됨
-            val month = nowDate.substring(5, 7)
-            return when (month) {
-                "01", "02" -> "경오1국"
-                "03", "04" -> "경오2국"
-                "05", "06" -> "경오3국"
-                "07", "08" -> "경오4국"
-                "09", "10" -> "경오5국"
-                "11", "12" -> "경오6국"
-                else -> nowTime
-            }
-        } else {
-            return "경오7국"
+        // nowDate의 month값이 이상해서 항상 else로 처리됨 => "갑자1국"이 리턴됨
+        val month = nowDate.substring(5, 7)
+        return when (month) {
+            "01", "02" -> "경오1국"
+            "03", "04" -> "경오2국"
+            "05", "06" -> "경오3국"
+            "07", "08" -> "경오4국"
+            "09", "10" -> "경오5국"
+            "11", "12" -> "경오6국"
+            else -> nowTime
         }
     }
 
     fun getTyB(): String {
-        val timeDataOne = _getTimeDataOne(nowDate)
-        var result = timeDataOne.first().ty12
+        val timeDataOne = _getTimeDataOne()
+        val result = timeDataOne.ty12
 
         val nowTime = LocalDateTime.now()
         return when (nowTime.hour) {
             // 시간은 항상 0보다 크거나 같으므로 항상 "갑자1국"이 반환됨
             // 2~4, 14~16 시간대가 빠져있음
-            in 0 until 2 ->  timeDataOne.first().ty1
-            in 2 until 4 -> timeDataOne.first().ty2
-            in 4 until 6 -> timeDataOne.first().ty3
-            in 6 until 8 -> timeDataOne.first().ty4
-            in 8 until 10 -> timeDataOne.first().ty5
-            in 10 until 12 -> timeDataOne.first().ty6
-            in 12 until 14 -> timeDataOne.first().ty7
-            in 14 until 16 -> timeDataOne.first().ty8
-            in 16 until 18 -> timeDataOne.first().ty9
-            in 18 until 20 -> timeDataOne.first().ty10
-            in 20 until 22 -> timeDataOne.first().ty11
-            in 22 until 24 -> timeDataOne.first().ty12
+            in 0 until 2 -> timeDataOne.ty1
+            in 2 until 4 -> timeDataOne.ty2
+            in 4 until 6 -> timeDataOne.ty3
+            in 6 until 8 -> timeDataOne.ty4
+            in 8 until 10 -> timeDataOne.ty5
+            in 10 until 12 -> timeDataOne.ty6
+            in 12 until 14 -> timeDataOne.ty7
+            in 14 until 16 -> timeDataOne.ty8
+            in 16 until 18 -> timeDataOne.ty9
+            in 18 until 20 -> timeDataOne.ty10
+            in 20 until 22 -> timeDataOne.ty11
+            in 22 until 24 -> timeDataOne.ty12
             else -> result
         }
     }
 
     // 인자인 nowDate 값을 사용하지 않음
     // timeDataOne 리스트에 YukymTimeModel의 인스턴스를 25개 넣어서 리턴
-    private fun _getTimeDataOne(nowDate: String): List<YukymTimeModel> {
-        val timeDataOne = mutableListOf<YukymTimeModel>()
-        for (i in 0..24) {
-            timeDataOne.add(YukymTimeModel())
-        }
-        return timeDataOne
+    private fun _getTimeDataOne(): YukymTimeModel {
+//        val timeDataOne = mutableListOf<YukymTimeModel>()
+//        for (i in 0..24) {
+//            timeDataOne.add(YukymTimeModel())
+//        }
+
+        return YukymTimeModel()
     }
 }
 
